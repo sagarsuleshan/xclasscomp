8e25449c89f7fd8d957c579d73f36dab9e7139cd
sagarsuleshan <sagarsuleshan@gmail.com>
2025-07-08 02:49:52 +0530

    Final Commit

---
+++ src/CounterApp.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
